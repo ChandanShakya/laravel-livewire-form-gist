01bd0f9a8536f6bfc263f3100a5c67e4a3bbe5a8
Chandan Shakya <ZXY-CC-3ag13@protonmail.com>
2025-07-13 21:20:02 +0545

    Add PostForm Livewire form object

---
+++ app/Livewire/Forms/PostForm.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Livewire\Forms;
+
+use Livewire\Attributes\Validate;
+use App\Models\Post;
+use Livewire\Form;
+
+class PostForm extends Form
+{
+    #[Validate('required|min:5')]
+    public $title = '';
+
+    #[Validate('required|min:5')]
+    public $content = '';
+
+    public function store()
+    {
+        $this->validate();
+        Post::create($this->all());
+        $this->reset();
+    }
+}
